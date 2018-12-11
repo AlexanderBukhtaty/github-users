92124fca75d93c32bdc4ab9f90278f59b37df2a4
Alexandr Bukhtatyy <buhtatyalexander90@gmail.com>
2018-12-11 22:28:49 +0600

    Сервис по работе с пользователями github и биндинги

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { GithubUsersService } from '@app/services/github-users.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'githubUsers';
+  
+  public state = {
+    loading: false
+  };
+
+  public form = {
+    term: ''
+  }
+  
+  public users = [];
+  
+  constructor(
+    private githubUsersService: GithubUsersService
+  ) {
+
+  }
+  
+  onSubmitForm() {
+    this.state.loading = true;
+    let params = new HttpParams()
+    .set('q',this.form.term)
+    .set('page', '1')
+    .set('per_page', '10');
+    this.githubUsersService.search(params).toPromise().then((response) => {
+      this.users = response.items;
+      this.state.loading = false;
+    })
+  }
 }
---
+++ src/app/services/github-users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface IQueryResponse<T> {
+  total_count: number;
+  incomplete_results: boolean;
+  items: T[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubUsersService {
+  
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  search(params: HttpParams): Observable<IQueryResponse<any>>{
+    return this.httpClient.get<IQueryResponse<any>>('http://api.github.com/search/users', {params});
+  }
+}
