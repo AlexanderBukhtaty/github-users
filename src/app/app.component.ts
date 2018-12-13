327f67d6e9ead66028ee98025c60bf97241a8e52
Alexandr Bukhtatyy <buhtatyalexander90@gmail.com>
2018-12-13 16:58:06 +0600

    Перенес логику на RXJs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpParams } from '@angular/common/http';
 import { GithubUsersService } from '@app/services/github-users.service';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { BehaviorSubject, Observable, combineLatest } from 'rxjs';
+import { map, tap, distinctUntilChanged, filter, switchMap,  } from 'rxjs/operators';
+
+interface IPagination {
+  page: number;
+  perPage: number;
+  totalItems: number;
+}
+
+const DEFAULT_PAGINATION: IPagination = { 
+  page: 1,
+  perPage: 10,
+  totalItems: 0 
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
-  public state = {
-    loading: false,
-    pagination: {
-      page: 1,
-      perPage: 10,
-      totalItems: 0
-    }
-  };
-
   public searchForm: FormGroup;
-  public users = [];
+
+  public users$: Observable<Array<any>>;
+
+  private loading$$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  public loading$: Observable<boolean> = this.loading$$.asObservable();
+  
+  private submittingForm$$: BehaviorSubject<FormGroup>;
+  public submittingForm$: Observable<FormGroup>;
+  
+  private pagination$$: BehaviorSubject<IPagination>;
+  public pagination$: Observable<IPagination>;
   
   constructor(
     private githubUsersService: GithubUsersService,
     private formBuilder: FormBuilder
-  ) {
+  ) {}
+  
+
+  ngOnInit () {
+    
     this.searchForm = this.formBuilder.group({
       term: ['', Validators.required]
-    })
-  }
+    });
 
-  loadData() {
-    this.state.loading = true;
-    let params = new HttpParams()
-      .set('q', this.searchForm.value.term)
-      .set('page', this.state.pagination.page.toString())
-      .set('per_page', this.state.pagination.perPage.toString());
-    this.githubUsersService.search(params).toPromise().then((response) => {
-      this.users = response.items;
-      this.state.pagination.totalItems = response.total_count;
-      this.state.loading = false;
-    }).catch(() => {
-      console.log('Something went wrong!');
-      this.state.loading = false;
-    })
+    this.submittingForm$$ = new BehaviorSubject(this.searchForm);
+    this.pagination$$ = new BehaviorSubject(DEFAULT_PAGINATION);
+    this.submittingForm$ = this.submittingForm$$.asObservable();
+    this.pagination$ = this.pagination$$.asObservable();
+
+    this.users$ = combineLatest(
+      this.submittingForm$$,
+      this.pagination$$.pipe(distinctUntilChanged((prev, next) => {
+        return prev.page === next.page && prev.perPage === next.perPage;
+      }))
+      ).pipe(
+        filter(([form, pagination]) => form.valid),
+        switchMap(([form, pagination]) => {
+          let params = new HttpParams()
+            .set('q', form.value.term)
+            .set('page', pagination.page.toString())
+            .set('per_page', pagination.perPage.toString());
+          return this.githubUsersService.search(params).pipe(
+            tap(result => {
+              this.loading$$.next(false);
+              this.pagination$$.next({ ...this.pagination$$.value, totalItems: result.total_count});
+            }),
+            map(result => result.items)
+          );
+        })
+    );
   }
 
   onSubmitForm() {
-    if (this.searchForm.valid && !this.state.loading) {
-      this.loadData()
-    }
+    this.loading$$.next(true);
+    this.pagination$$.next(DEFAULT_PAGINATION);
+    this.submittingForm$$.next(this.searchForm);
   }
 
   onChangePage(page: number) {
-    this.state.pagination.page = page;
-    this.loadData()
+    this.loading$$.next(true);
+    this.pagination$$.next({ 
+      ...this.pagination$$.value,
+      page: page
+    });
   }
 }
